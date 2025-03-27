1ea4d8f1b82614644193331a0ff4d4248d90db4a
Adriano de Alencar <dinhoalencaraa@gmail.com>
2025-03-27 15:58:56 -0300

    first commit

---
+++ src/main/java/dev/euvei/Cadastro/CadastroComApiApplication.java
@@ -0,0 +1,13 @@
+package dev.euvei.Cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroComApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroComApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/euvei/Cadastro/CadastroComApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.euvei.Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroComApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
